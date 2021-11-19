69d49c3bf2cdf35fd104d38e8717d2bf92f252e3
Anderson Ferreira Alves <andersonamericasul07@gmail.com>
2021-11-19 01:45:32 -0300

    Atividade 6, javascript

---
+++ Javascript/atividade_6/script.js
@@ -0,0 +1,16 @@
+document.getElementById('label-info-pessoais').innerHTML = 'Informações pessoais'
+document.getElementById('nome').innerHTML = 'Nome: Anderson Ferreira Alves'
+document.getElementById('idade').innerHTML = 'Idade: 25 anos'
+document.getElementById('estado').innerHTML = 'Estado Civil: Solteiro'
+document.getElementById('foto').setAttribute('src', 'assets/img/perfil.jpeg')
+document.getElementById('label-hobby').innerHTML = 'Hobby'
+document.getElementById('titulo-hobby').innerHTML = 'Assistir Animes'
+document.getElementById('descricao-hobby').innerHTML = 'Informações pessoais'
+document.getElementById('label-links-hobby').innerHTML = 'Links'
+document.getElementById('link1-hobby').innerHTML = 'Jujutsu Kaisen'
+document.getElementById('link1-hobby').setAttribute('href', 'https://www.crunchyroll.com/pt-br/jujutsu-kaisen')
+document.getElementById('link2-hobby').innerHTML = 'Digimon'
+document.getElementById('link2-hobby').setAttribute('href', 'https://pt.wikipedia.org/wiki/Digimon')
+document.getElementById('label-imagens-hobby').innerHTML = 'Imagens'
+document.getElementById('img1-hobby').setAttribute('src', 'assets/img/digimon.jpg')
+document.getElementById('img2-hobby').setAttribute('src', 'assets/img/jujutsu.jpg')
