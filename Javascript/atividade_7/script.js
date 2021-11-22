dbfc8ef6495ead4d7a514a2a4a0ce9081d321eb2
Anderson Ferreira Alves <andersonamericasul07@gmail.com>
2021-11-22 09:59:33 -0300

    Atividade 7, javscript corrigida

--- Javascript/atividade_7/script.js
+++ Javascript/atividade_7/script.js
@@ -1,10 +1,10 @@
-document.querySelector('header nav > :nth-child(1) li a').innerHTML = 'Sobre mim'
+document.querySelector('header nav ul > :nth-child(1) a').innerHTML = 'Sobre mim'
 
-document.querySelector('header nav > :nth-child(2) li a').innerHTML = 'Sobre a banda'
+document.querySelector('header nav ul > :nth-child(2) a').innerHTML = 'Sobre a banda'
 
-document.querySelector('header nav > :nth-child(3) li a').innerHTML = 'Discografia'
+document.querySelector('header nav ul > :nth-child(3) a').innerHTML = 'Discografia'
 
-document.querySelector('header nav > :nth-child(4) li a').innerHTML = 'Músicas Preferidas'
+document.querySelector('header nav ul > :nth-child(4) a').innerHTML = 'Músicas Preferidas' 
 
 document.querySelector('#sobre-mim h2').innerHTML = 'Sobre mim'
 
@@ -12,6 +12,6 @@ document.querySelector('#section-banda-favorita h2').innerHTML = 'Banda Favorita
 
 document.querySelector('#section-discografia h2').innerHTML = 'Discografia'
 
-document.querySelector('#section-musicas-preferidas > h2').innerHTML = 'Músicas Preferidas'
+document.querySelector('#section-musicas-preferidas h2').innerHTML = 'Músicas Preferidas'
 
 
