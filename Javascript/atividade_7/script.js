72c1b2521a74e2b226d8739084a94e7967ac4da5
Anderson Ferreira Alves <andersonamericasul07@gmail.com>
2021-11-22 09:21:20 -0300

    Atividade 7, javacript

---
+++ Javascript/atividade_7/script.js
@@ -0,0 +1,17 @@
+document.querySelector('header nav > :nth-child(1) li a').innerHTML = 'Sobre mim'
+
+document.querySelector('header nav > :nth-child(2) li a').innerHTML = 'Sobre a banda'
+
+document.querySelector('header nav > :nth-child(3) li a').innerHTML = 'Discografia'
+
+document.querySelector('header nav > :nth-child(4) li a').innerHTML = 'Músicas Preferidas'
+
+document.querySelector('#sobre-mim h2').innerHTML = 'Sobre mim'
+
+document.querySelector('#section-banda-favorita h2').innerHTML = 'Banda Favorita: Queem'
+
+document.querySelector('#section-discografia h2').innerHTML = 'Discografia'
+
+document.querySelector('#section-musicas-preferidas > h2').innerHTML = 'Músicas Preferidas'
+
+
